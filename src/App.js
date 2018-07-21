702ea09fb12856ab2eba459676d41b99a0b41434
kevinbai0 <kevin.c.bai0@gmail.com>
2018-07-21 16:51:52 -0400

    Created base ui

--- src/App.js
+++ src/App.js
@@ -5,15 +5,8 @@ import InputField from "./InputField.js";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { show: false };
-
-    this.toggleDiv = this.toggleDiv.bind(this);
+    this.state = { show: true };
   }
-
-  toggleDiv = () => {
-    this.setState({ show: true });
-    console.log(this.state);
-  };
 
   render() {
     return (
@@ -22,7 +15,6 @@ class App extends Component {
           <h2>Powercard Finder</h2>
         </div>
         <div className="App-intro">
-          <button onClick={this.toggleDiv}>New Powercard</button>
           <InputField isShown={this.state.show} />
         </div>
       </div>
--- src/InputField.js
+++ src/InputField.js
@@ -1,24 +1,52 @@
 import React, { Component } from "react";
-import "./InputFieldStyleSheet.css";
+import "./InputFieldStylesheet.css";
 
 export default class InputField extends Component {
   constructor(props) {
     super(props);
+    this.state = { cardType: "White" };
   }
 
   render() {
     if (this.props.isShown) {
-      return (
-        <div className="cardEditorTemplate">
-          <textarea className="cardInputTextArea"/>
+      if (this.state.cardType == "White") {
+        return (
+        <div className="cardLight cardEditorTemplate">
+          <textarea className="cardLight cardInputTextArea" placeholder="Enter card..."/>
+          <div className="cardLight cardSelectorOptionArea">
+            <div className="cardLight cardSelector" id="whiteSelector" onClick={this.handleCardChange.bind(this, "White")}>White</div>
+            <div className="cardLight cardSelector" id="blackSelector" onClick={this.handleCardChange.bind(this, "Black")}>Black</div>
+            <div className="cardLight cardSelector" id="submitSelector" onClick={this.handleCardChange.bind(this, "Submit")}>Submit</div>
+          </div>
         </div>
-      );
+        );
+      }
+      else {
+        return (
+        <div className="cardDark cardEditorTemplate">
+          <textarea className="cardDark cardInputTextArea" placeholder="Enter card..."/>
+          <div className="cardDark cardSelectorOptionArea">
+            <div className="cardDark cardSelector" id="whiteSelector" onClick={this.handleCardChange.bind(this, "White")}>White</div>
+            <div className="cardDark cardSelector" id="blackSelector" onClick={this.handleCardChange.bind(this, "Black")}>Black</div>
+            <div className="cardDark cardSelector" id="submitSelector" onClick={this.handleCardChange.bind(this, "Submit")}>Submit</div>
+          </div>
+        </div>
+        )
+      }
     } else {
       return <div />;
     }
   }
 
-  checkInputField = () => {
-
-  };
+  handleCardChange(cardType) {
+    console.log("Card change");
+    if (cardType === "White") {
+      this.setState({cardType: "White"});
+      console.log("White");
+    }
+    else if (cardType === "Black") {
+      this.setState({cardType: "Black"});
+      console.log("Black");
+    }
+  }
 }
